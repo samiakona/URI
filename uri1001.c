596a91839c3b24fbec99c0d74e075ae6106b0fbe
samiakona <69978344+samiakona@users.noreply.github.com>
2020-11-07 16:47:42 +0600

    Add files via upload

---
+++ uri1001.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//Summation of two number
+
+int main()
+{
+    int X, A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A+B;
+    printf("X = %d\n", X);
+    return 0;
+}
---
+++ uri1003.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int A, B, S;
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    S= A + B;
+    printf("SOMA = %d\n", S);
+
+
+    return 0;
+}
